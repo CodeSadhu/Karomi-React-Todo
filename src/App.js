b8e1607de0f750e0183ac817b9f0682331f1a498
Atharva Patwardhan <patwardhanatharva6@gmail.com>
2020-12-20 17:05:32 +0530

    Before adding state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css'
+// Components Imported from here
+import Form from './components/Form.js';
+import TodoList from './components/TodoList.js';
 
 function App() {
 	return (
@@ -7,6 +10,8 @@ function App() {
 			<header>
 				<h1>My Todo List</h1>
 			</header>
+			<Form />
+			<TodoList />
 		</div>
 	);
 }
